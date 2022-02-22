2c1a9a2b785624aefc766e8041b36a641a7d19b9
sappiajeremias <jeremiassappia@gmail.com>
2022-02-22 17:33:50 -0300

    Scripts cargados

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # TestingScripts
 Repository in which I am going to work with JavaScript to learn how it is used together with an HTML file.
+
+
+# Programmer
+
+Name: Jeremias Sappia
+
+Mail jeremiassappia@gmail.com
+
+Degree: University Technician student in Web Development
+
+LinkedIn: <a href="https://www.linkedin.com/in/jeremias-sappia-a83028214/" target="_blank"><p>Jeremias Sappia</p></a>
---
+++ scripts.js
@@ -0,0 +1,30 @@
+/* 
+    const = Constante
+    let = Variable de bloque
+    var = Variable comun, mejor usar let
+
+    Ej:
+        let numero = 15;
+        const iva = 21;
+
+    alert(): Alerta mostrada en pantalla
+        alert("Hola bienvenido a la pagina");
+
+    prompt: Ingreso de datos por el usuario
+        let nombre = prompt("Decime tu nombre");
+    
+    document.write: Un print en la pagina, no como alerta
+        document.write("Hola papa");
+        let frase = "" + numero + iva;
+        document.write(frase);
+
+    Concatenacion
+        - Doble comillas para que tome todo como string y concatene en vez de sumar
+        - .concat(): funcion para concatenar dos strings
+        - Backticks y ${}:
+            let nombre = "Jere";
+            let saludo = `Hola ${nombre}, como estas?`;
+
+*/
+
+//Historia de Cofla, capitulo 1:
